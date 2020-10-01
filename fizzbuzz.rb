f4710b2bd2dbec2332698b0fb97d799d428b8363
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-01 02:13:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(number)
+  
+  
 end
+
+
+
+fizz_5 = fizzbuzz(5)
+fizz_3 = fizzbuzz(3)
+fizz_15 = fizzbuzz(15)
