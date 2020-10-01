a69361a61da492b6195a4950464e15d25f31ca56
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-01 02:29:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if(number = 3)
+  if(number == 3)
     number = fizzbuzz_3
     puts "Fizz"
   elsif
-    (number = 5)
+    (number == 5)
     number = fizzbuzz_5
     puts "Buzz"
   elsif
-    (number = 15)
+    (number == 15)
     number = fizzbuzz_15
     puts "Fizz Buzz"
   else
